1ca165b826f28853dd411b8e7df9cf50e5fdcbb2
geyi <geyichn@126.com>
2020-10-16 23:00:32 +0800

    Semaphore

---
+++ src/main/java/com/airing/thread/SemaphoreTest.java
@@ -0,0 +1,38 @@
+package com.airing.thread;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+public class SemaphoreTest {
+
+    public static void main(String[] args) {
+        Semaphore semaphore = new Semaphore(1);
+
+        new Thread(() -> {
+            try {
+                semaphore.acquire();
+                System.out.println(Thread.currentThread().getName() + " running");
+                TimeUnit.SECONDS.sleep(1);
+                System.out.println(Thread.currentThread().getName() + " running");
+            } catch (Exception e) {
+
+            } finally {
+                semaphore.release();
+            }
+        }).start();
+
+        new Thread(() -> {
+            try {
+                semaphore.acquire();
+                System.out.println(Thread.currentThread().getName() + " running");
+                TimeUnit.SECONDS.sleep(1);
+                System.out.println(Thread.currentThread().getName() + " running");
+            } catch (Exception e) {
+
+            } finally {
+                semaphore.release();
+            }
+        }).start();
+    }
+
+}
