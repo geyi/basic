3b3b889e69f12696767d4b02b9f99e53ff9d2313
geyi <geyichn@126.com>
2020-10-16 23:08:44 +0800

    Exchanger

---
+++ src/main/java/com/airing/thread/ExchangerTest.java
@@ -0,0 +1,34 @@
+package com.airing.thread;
+
+import java.util.concurrent.Exchanger;
+import java.util.concurrent.TimeUnit;
+
+public class ExchangerTest {
+
+    public static void main(String[] args) {
+        Exchanger<String> exchanger = new Exchanger<>();
+
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                String localValue = Thread.currentThread().getName();
+                String value = exchanger.exchange(localValue);
+                System.out.println(localValue + " " + value);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "t0").start();
+
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+                String localValue = Thread.currentThread().getName();
+                String value = exchanger.exchange(localValue);
+                System.out.println(localValue + " " + value);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "t1").start();
+    }
+
+}
