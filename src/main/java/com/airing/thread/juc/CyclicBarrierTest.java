653d816fc4ab540b54ac7bf7cdf9b6482d715722
geyi <geyichn@126.com>
2021-04-04 19:51:23 +0800

    CyclicBarrier

--- src/main/java/com/airing/thread/juc/CyclicBarrierTest.java
+++ src/main/java/com/airing/thread/juc/CyclicBarrierTest.java
@@ -6,19 +6,21 @@ import java.util.concurrent.CyclicBarrier;
 public class CyclicBarrierTest {
 
     public static void main(String[] args) {
-        CyclicBarrier barrier = new CyclicBarrier(20, () -> {
+        CyclicBarrier barrier = new CyclicBarrier(5, () -> {
             System.out.println("出发！");
         });
 
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 20; i++) {
             new Thread(() -> {
                 try {
+                    System.out.println(Thread.currentThread().getName() + " 就位");
                     barrier.await();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } catch (BrokenBarrierException e) {
                     e.printStackTrace();
                 }
+                System.out.println(Thread.currentThread().getName() + "到咯");
             }).start();
         }
     };
