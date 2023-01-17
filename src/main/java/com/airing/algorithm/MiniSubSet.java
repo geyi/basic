d770388e104883d84b786e1f8f2d579d250a5891
geyi <geyichn@126.com>
2023-01-17 14:00:06 +0800

    添加代码注释

--- src/main/java/com/airing/algorithm/MiniSubSet.java
+++ src/main/java/com/airing/algorithm/MiniSubSet.java
@@ -19,12 +19,12 @@ public class MiniSubSet {
         List<String> ret = new ArrayList<>();
         int len = arr.length;
         int maxSubLen = len - 1;
-        for (int i = 1; i <= maxSubLen; i++) {
-            int limit = len - i + 1;
+        for (int i = 1; i <= maxSubLen; i++) { // 最小子级从1开始
+            int limit = len - i + 1; // 保证从索引为(limit - 1)的位置开始也能够取到最小子级（获取最小子级时能够遍历到的最后一个元素的位置）
             for (int j = 0; j < limit; j++) {
                 int sum = 0;
                 StringBuilder sb = new StringBuilder();
-                for (int k = j; k < j + i; k++) {
+                for (int k = j; k < j + i; k++) { // j 表示获取最小子级时的开始位置，j + i 表示获取最小子级时的结束位置（不包含）
                     sum += arr[k];
                     sb.append(",").append(arr[k]);
                 }
