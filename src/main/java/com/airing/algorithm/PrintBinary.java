6df53d55ffc760e9d8eee83905a2057272667067
geyi <geyichn@126.com>
2023-01-17 14:01:45 +0800

    删除无用的代码

--- src/main/java/com/airing/algorithm/PrintBinary.java
+++ src/main/java/com/airing/algorithm/PrintBinary.java
@@ -4,7 +4,6 @@ public class PrintBinary {
     public static void main(String[] args) {
         int i = Integer.MAX_VALUE;
         print(i);
-        new Sort();
     }
 
     public static void print(int i) {
