e89b7e9c1f9f0200f287fbd7827dc85aaceeb843
geyi <geyichn@126.com>
2023-08-31 15:27:30 +0800

    更新用于分隔的字符

--- src/main/java/com/airing/jvm/clazz/loader/ClassLoaderScope.java
+++ src/main/java/com/airing/jvm/clazz/loader/ClassLoaderScope.java
@@ -4,13 +4,13 @@ public class ClassLoaderScope {
 
     public static void main(String[] args) {
         String bootClassPath = System.getProperty("sun.boot.class.path");
-        System.out.println(bootClassPath.replaceAll(":", System.lineSeparator()));
+        System.out.println(bootClassPath.replaceAll(";", System.lineSeparator()));
         System.out.println("------------");
         String extClassPath = System.getProperty("java.ext.dirs");
-        System.out.println(extClassPath.replaceAll(":", System.lineSeparator()));
+        System.out.println(extClassPath.replaceAll(";", System.lineSeparator()));
         System.out.println("------------");
         String appClassPath = System.getProperty("java.class.path");
-        System.out.println(appClassPath.replaceAll(":", System.lineSeparator()));
+        System.out.println(appClassPath.replaceAll(";", System.lineSeparator()));
         System.out.println("------------");
     }
 
