33b477aac1ae2873f60b55cd8d6c5c3fbf7c9c5f
geyi <geyichn@126.com>
2023-10-13 10:26:53 +0800

    添加代码注释

--- src/main/java/com/airing/jvm/clazz/loader/ClassFileFormat.java
+++ src/main/java/com/airing/jvm/clazz/loader/ClassFileFormat.java
@@ -1,5 +1,12 @@
 package com.airing.jvm.clazz.loader;
 
+/**
+ * 使用插件 BinEd - Binary/Hexadecimal Editor 查看class文件的二进制内容
+ * 使用查件 jclasslib Bytecode Viewer 查看class文件的内容
+ *
+ * @author GEYI
+ * @date 2023年10月13日 9:56
+ */
 public class ClassFileFormat {
     public static void main(String[] args) {
         Object obj = new Object();
