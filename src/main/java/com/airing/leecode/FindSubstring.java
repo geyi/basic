c76fa32376ef0c986b655412fc560a869f0acd97
geyi <geyichn@126.com>
2024-08-19 16:13:45 +0800

    leecode

--- src/main/java/com/airing/leecode/FindSubstring.java
+++ src/main/java/com/airing/leecode/FindSubstring.java
@@ -1,13 +1,14 @@
 package com.airing.leecode;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class FindSubstring {
     public static void main(String[] args) {
-        System.out.println(findSubstring("barfoothefoobarman", new String[]{"foo","bar"}));
+        System.out.println(findSubstring("lingmindraboofooowingdingbarrwingmonkeypoundcake", new String[]{"fooo","barr","wing","ding","wing"}));
     }
 
     public static List<Integer> findSubstring(String s, String[] words) {
@@ -18,28 +19,30 @@ public class FindSubstring {
             return new ArrayList<>(0);
         }
 
-        Set<String> set = new HashSet<>();
-        for (String word : words) {
-            set.add(word);
-        }
+        List<String> wordList = new ArrayList<>(Arrays.asList(words));
 
         int start = 0;
         int end = 0;
         int limit = 0;
         List<Integer> list = new ArrayList<>();
-        while (start <= s.length() - subStrLen && limit < wordLen) {
+        while (start <= s.length() - subStrLen && limit < len) {
             int left = start + limit *  wordLen;
             int right = start + limit *  wordLen + wordLen;
-            if (set.contains(s.substring(left, right))) {
+            if (wordList.remove(s.substring(left, right))) {
                 if (limit == len - 1) {
                     list.add(start);
-                    start = right;
+                    start = start + 1;
                     limit = 0;
+                    wordList = new ArrayList<>(Arrays.asList(words));
                 } else {
                     ++limit;
                 }
             } else {
-                ++start;
+                start = start + 1;
+                limit = 0;
+                if (wordList.size() < len) {
+                    wordList = new ArrayList<>(Arrays.asList(words));
+                }
             }
 
         }
